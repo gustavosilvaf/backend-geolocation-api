be436da11ed519e80a47834493e409adc7d3a817
gustavosilvaf <gustavofsilva@email.com>
2021-11-10 12:52:20 -0300

    Validate body]

--- src/geolocation/dto/send-address.dto.ts
+++ src/geolocation/dto/send-address.dto.ts
@@ -1,8 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 
 export class SendAddressDto {
   @ApiProperty({
     description: 'Array of Addresses ',
   })
-  readonly addresses: string[];
+  @IsNotEmpty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @ArrayMinSize(1)
+  @IsString({ each: true })
+  public addresses: string[];
 }
--- src/geolocation/interfaces/geolocation.interface.ts
+++ src/geolocation/interfaces/geolocation.interface.ts
@@ -1,7 +1,16 @@
+import { ValidationError, ValidatorOptions } from 'class-validator';
+
 export interface Locale {
   locale: string;
   location: {
     lat: number;
     lng: number;
   };
+}
+
+export interface ValidationPipeOptions extends ValidatorOptions {
+  transform?: boolean;
+  disableErrorMessages?: boolean;
+  enableDebugMessages: boolean;
+  exceptionFactory?: (errors: ValidationError[]) => any;
 }
